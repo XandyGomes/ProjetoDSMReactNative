960e77a28fbc939e607a6fc20b1aafde5e64ef3e
XandyGomes <x3.computadores@hotmail.com>
2023-04-01 09:19:44 -0300

    Aula.01.04.2023

--- src/pages/login.js
+++ src/pages/login.js
@@ -9,7 +9,7 @@ const Login = () => {
   const navigation = useNavigation();
 
   const handleLogin = () => {
-    if(email === 'admin' && password === '1234'){
+    if(email === '' && password === ''){
       navigation.navigate('main')
     } else {
       alert('Email ou senha inválidos!')
--- src/pages/main.js
+++ src/pages/main.js
@@ -1,8 +1,42 @@
-import React from 'react';
-import { Container, Form, Input, SubmitButton } from './styles';
+import React, { Component } from 'react';
+
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
-export default function Main() {
+import api from '../services/api';
+
+import { Container, Form, Input, SubmitButton } from './styles';
+
+export default class Main extends Component {
+
+  state = {
+    newUser: '',
+    users: [],
+  };
+
+  handleAddUser = async () => {
+    
+    const { users, newUser } = this.state;
+
+    const response = await api.get(`/users/${newUser}`);
+
+    const data = {
+      name: response.data.name,
+      login: response.data.login,
+      bio: response.data.bio,
+      avatar: response.data.avatar_url,
+    };
+
+    this.setState({
+      users: [...users, data],
+      newUser:'',
+    });
+
+    console.log(data)
+  };
+
+  render() {
+    const { users, newUser } = this.state;
+
     return (
       <Container>
         <Form>
@@ -10,11 +44,16 @@ export default function Main() {
             autoCorrect={false}
             autoCapitalize='none'
             placeholder='Adicionar usuário'
+            value={newUser}
+            onChangeText={text=> this.setState({ newUser: text })}
+            returnKeyType='send'
+            onSubmitEditing={this.handleAddUser}
           />
-          <SubmitButton>
+          <SubmitButton onPress={this.handleAddUser}>
             <Icon name='add' size={20} color='#fff' />
           </SubmitButton>
         </Form>
       </Container>
     );
+  }
 }
