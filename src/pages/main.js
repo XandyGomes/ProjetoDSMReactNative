3cc54b8f960c7170f85178eef6ee4acfda0b8be9
XandyGomes <x3.computadores@hotmail.com>
2023-04-28 22:26:24 -0300

    Aula.28.04.2023

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Keyboard } from 'react-native'
+import { Keyboard, ActivityIndicator } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import api from '../services/api';
 import { Container, Form, Input, SubmitButton, List, User, Avatar, Name, Bio, ProfileButton, ProfileButtonText } from './styles';
@@ -9,11 +10,30 @@ export default class Main extends Component {
   state = {
     newUser: '',
     users: [],
+    loading: false,
   };
+
+  async componentDidMount() {
+    const users = await AsyncStorage.getItem('users');
+    
+    if (users) {
+      this.setState({ users: JSON.parse(users) });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    const { users } = this.state;
+
+    if (prevState.users !== users) {
+      AsyncStorage.setItem('users', JSON.stringify(users));
+    }
+  }
 
   handleAddUser = async () => {
 
     const { users, newUser } = this.state;
+
+    this.setState({ loading: true });
 
     const response = await api.get(`/users/${newUser}`);
 
@@ -27,6 +47,7 @@ export default class Main extends Component {
     this.setState({
       users: [...users, data],
       newUser: '',
+      loading: false,
     });
 
     Keyboard.dismiss();
@@ -35,7 +56,7 @@ export default class Main extends Component {
   };
 
   render() {
-    const { users, newUser } = this.state;
+    const { users, newUser, loading } = this.state;
 
     return (
       <Container>
@@ -49,8 +70,8 @@ export default class Main extends Component {
             returnKeyType='send'
             onSubmitEditing={this.handleAddUser}
           />
-          <SubmitButton onPress={this.handleAddUser}>
-            <Icon name='add' size={20} color='#fff' />
+          <SubmitButton loading={loading} onPress={this.handleAddUser}>
+            {loading ? (<ActivityIndicator color='#fff' />) : (<Icon name='add' size={20} color='#fff' />)}
           </SubmitButton>
         </Form>
         <List
